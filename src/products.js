a078db81753b3a8787363ac91fc05538d9cbdca1
brian-ambani <brainambani1@gmail.com>
2024-05-27 22:03:36 +0300

    created shopping cart project

---
+++ src/products.js
@@ -0,0 +1,66 @@
+import product1 from "./assets/shoe1.jpg";
+import product2 from "./assets/shoe2.jpg";
+import product3 from "./assets/shoe8.jpg";
+import product4 from "./assets/shoe3.jpg";
+import product5 from "./assets/shoe4.jpg";
+import product6 from "./assets/shoe5.jpg";
+import product7 from "./assets/shoe6.jpg";
+import product8 from "./assets/shoe7.jpg";
+import product9 from "./assets/shoe9.jpg";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "Nike",
+    price: 999.0,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Vans Old Skool",
+    price: 1999.0,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Nike runners",
+    price: 699.0,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "WLS Van Gogh Denim Jacket",
+    price: 228.0,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "LED Light Strips",
+    price: 19.99,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "SPECTRUM LS TEE",
+    price: 68.0,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "Classy women",
+    price: 120.0,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "Sketchers",
+    price: 40.0,
+    productImage: product8,
+  },
+  {
+    id: 9,
+    productName: "Airforce",
+    price: 40.0,
+    productImage: product9,
+  },
+];
